c8782839ede43ccd1373bd1a01f5a75e516ea9e0
Yasmin Louren Schultz Carneiro <145464550+yasminschultz@users.noreply.github.com>
2023-10-18 14:00:27 -0300

    Add files via upload

--- aula01/exp01.php
+++ aula01/exp01.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
+    <title>exp01</title>
 </head>
 <body>
     <?php
-echo "arquivo em php";
+echo "teste 01";
     ?>
+    <h1>teste de alteração do github</h1>
     
 </body>
 </html>
